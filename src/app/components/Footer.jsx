35b9ff524e4c8dce4c1f7d1093c15efaff7b7eb8
Muhammad Henry Aditya <adityahenry2@gmail.com>
2025-06-27 09:48:49 +0800

    Fix: year creation

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
                 <Image src="/icon-removebg-preview.png" alt="Logo" width={100} height={100} />
             </div> */}
             <div className="container mx-auto text-center">
-                <p className="text-sm">© 2023 Handwritten OCR. All rights reserved.</p>
+                <p className="text-sm">© 2025 Handwritten OCR. All rights reserved.</p>
                 <p className="text-sm">Made with ❤️ by Muhammad Henry Aditya</p>
             </div>
         </footer>
